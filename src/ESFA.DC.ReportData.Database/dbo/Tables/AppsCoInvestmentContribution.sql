4dc36004cb072f387fcf28cb657b489f0ff64dbc
Hari Dupati <dupatihari@gmail.com>
2020-07-01 11:54:39 +0100

    100495 - Added primary keys to Period End tables and generated models

--- src/ESFA.DC.ReportData.Database/dbo/Tables/AppsCoInvestmentContribution.sql
+++ src/ESFA.DC.ReportData.Database/dbo/Tables/AppsCoInvestmentContribution.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[AppsCoInvestmentContribution]
 (
-	[Ukprn] INT NOT NULL , 
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+    [Ukprn] INT NOT NULL , 
     [ReturnPeriod] INT NOT NULL, 
     [LearnRefNumber] VARCHAR(12) NULL, 
     [UniqueLearnerNumber] BIGINT NULL,
@@ -38,9 +39,9 @@
     [CompletionEarningThisFundingYear] DECIMAL(15,5) NOT NULL, 
     [CompletionPaymentsThisFundingYear] DECIMAL(15,5) NOT NULL, 
     [EmployerCoInvestmentPercentage] DECIMAL(15,5) NULL, 
-    [ApplicableProgrammeStartDate] DATETIME NULL 
+    [ApplicableProgrammeStartDate] DATETIME NULL,
+    CONSTRAINT [PK_AppsCoInvestmentContribution] PRIMARY KEY ([Id])
 )
 
 GO
 
-CREATE CLUSTERED INDEX [IX_AppsCoInvestmentContribution_Ukprn_ReturnPeriod] ON [dbo].[AppsCoInvestmentContribution] ([Ukprn],[ReturnPeriod])
--- src/ESFA.DC.ReportData.Database/dbo/Tables/LearnerLevelViewReport.sql
+++ src/ESFA.DC.ReportData.Database/dbo/Tables/LearnerLevelViewReport.sql
@@ -1,4 +1,5 @@
 ﻿ CREATE TABLE [dbo].[LearnerLevelViewReport](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Ukprn] [int] NOT NULL,
 	[ReturnPeriod] [int] NOT NULL,
 	[PaymentLearnerReferenceNumber] [varchar](12) NOT NULL,
@@ -17,12 +18,7 @@
 	[IssuesAmount] [decimal](15, 5) NULL,
 	[ReasonForIssues] [varchar](max) NULL,
 	[PaymentFundingLineType] [varchar](max) NULL,
-	[RuleDescription] [varchar](max) NULL
+	[RuleDescription] [varchar](max) NULL,
+	CONSTRAINT [PK_LearnerLevelViewReport] PRIMARY KEY ([Id])
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-
-
-
-
-
-CREATE INDEX [IX_LearnerLevelViewReport_Column] ON [dbo].[LearnerLevelViewReport] ([Ukprn], [ReturnPeriod])
